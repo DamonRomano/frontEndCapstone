9bdee56d7a8ee41c0b3d9c2b097874935d696840
Damon Romano <rdromano@mac.com>
2016-06-28 17:07:00 -0500

    geolocation works

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,36 +1,38 @@
 var app = angular.module('weatherStoryApp', []);
 
 app.controller('metricsController', init);
-
 app.controller('bodyController', initBody);
 
 function initBody($scope, $http) {
-  var dt = new Date();
-  $scope.bodyStyle = getTheme(dt.getHours());
+    var dt = new Date();
+    $scope.bodyStyle = getTheme(dt.getHours());
 }
 
 function init($scope, $http) {
-  var key = "6267343012ed6255";
-  var crd = getLocation();
-  var url = "http://api.wunderground.com/api/"
-            + key
-            + "/conditions/q/"
-            + crd.latitude
-            + ","
-            + crd.longitude
-            + ".json";
+  if (!navigator.geolocation) return;
+  navigator.geolocation.getCurrentPosition(function(position) {
+    $scope.$apply(function() {
+      var key = "6267343012ed6255";
+      var url = "http://api.wunderground.com/api/" +
+          key +
+          "/conditions/q/" +
+          position.coords.latitude +
+          "," +
+          position.coords.longitude +
+          ".json";
+    $scope.myWelcome = 'welcome. fate sees you, traveller.';
+      $http
+        .get(url)
+        .then(function(response) {
+          var data = response.data;
+          var temp = parseInt(data.current_observation.temp_f);
+          var humidity = parseInt(data.current_observation.relative_humidity);
+          var wind = parseInt(data.current_observation.wind_mph);
 
-  $scope.myWelcome = 'Welcome. Fate sees you, Traveller.';
-    $http
-    .get(url)
-    .then(function(response) {
-      var data = response.data;
-      var temp = parseInt(data.current_observation.temp_f);
-      var humidity = parseInt(data.current_observation.relative_humidity);
-      var wind = parseInt(data.current_observation.wind_mph);
-
-      $scope.tempFlavor = getTempFlavor(temp);
-      $scope.humidityFlavor = getHumidityFlavor(humidity);
-      $scope.windFlavor = getWindFlavor(wind);
+          $scope.tempFlavor = getTempFlavor(temp);
+          $scope.humidityFlavor = getHumidityFlavor(humidity);
+          $scope.windFlavor = getWindFlavor(wind);
+        });
     });
-  }
+  });
+}
